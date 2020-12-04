2ba2e4dd953f1fbc15c107141baee209c30e0e6d
yuanmo <1445089819@qq.com>
2020-12-04 21:51:23 +0800

    sdf

--- 1006.cpp
+++ 1006.cpp
@@ -1,38 +1,77 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
-struct person {
+struct person
+{
     string id;
-    string signIntime;
-    string signOuttime;
+    int It;
+    int Ot;
 };
-bool com(const string&l,const string&r) {
-    if(stoi(l.substr(0,2))>stoi(r.substr(0,2))) {
-        return true;
-    } else if(stoi(l.substr(0,2))<stoi(r.substr(0,2))) {
-        return false;
-
-    } else {
-
-        if(stoi(l.substr(3,2))>stoi(r.substr(3,2))) {
-            return true;
-
-        } else if(stoi(l.substr(3,2))<stoi(r.substr(3,2))) {
-            return false;
-        } else {
-            if(stoi(l.substr(3,2))>stoi(r.substr(3,2))) {
-                return true;
-            } else if(stoi(l.substr(3,2))<stoi(r.substr(3,2))) {
-                return false;
-            } else {
-                return true;
-            }
-            
-        }
-    }
+int strTimetoInt(const string& strtime) {
+    return stoi(strtime.substr(0,2))*3600+stoi(strtime.substr(3,2))*60+stoi(strtime.substr(6,2));
 }
-int main() {
+
+vector<person> OutRoom,InRoom;
+vector<int> InOutTime;
+int main()
+{
     int pCount;
-    cin>>pCount;
+    cin >> pCount;
+    for(int i = 0;i<pCount;++i) {
+        person p;
+        string Itime,Otime;
+        cin>>p.id>>Itime>>Otime;
+        p.It = strTimetoInt(Itime);
+        p.Ot = strTimetoInt(Otime);
+        OutRoom.push_back(p);
+        InOutTime.push_back(p.It);
+        InOutTime.push_back(p.Ot);
+    }
+
+    sort(InOutTime.begin(),InOutTime.end());
+
+    for(int k=0;k<InOutTime.size();++k) {
+        int& X = InOutTime.at(k);
+        string WillInPerson{""},WillOutPerson{""};
+        for(int i = 0;i<OutRoom.size();++i) {          
+            if(X==OutRoom[i].It)
+            {
+                WillInPerson = OutRoom[i].id;
+                InRoom.push_back(OutRoom[i]);
+                OutRoom.erase(OutRoom.begin()+i);
+                break;
+            }
+        }
+        for(int j = 0;j<InRoom.size();++j)
+        {
+            if(X == InRoom[j].Ot)
+            {
+                WillOutPerson = InRoom[j].id;
+                OutRoom.push_back(InRoom[j]);
+                InRoom.erase(InRoom.begin()+j);
+                break;
+            }
+        }
+        if(WillInPerson != ""&&WillOutPerson!="")
+        {
+            continue;
+        }
+        if(WillOutPerson!="")
+        {
+            if(InRoom.size()==0)
+            std::cout<<WillOutPerson;
+
+        }
+
+        if(WillInPerson!="")
+        {
+            if(InRoom.size()==1)
+            std::cout<<WillInPerson;
+        }
+        
+        if(k!=InOutTime.size()-1)
+        std::cout<<" ";
+    }
 }
