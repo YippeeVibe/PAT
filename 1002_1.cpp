eca56c6c70785760732343d7a4b5496a0ab60a4e
yuanmo <1445089819@qq.com>
2020-12-04 14:03:54 +0800

    new

--- 1002_1.cpp
+++ 1002_1.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<sstream>
 struct poly
 {	
 	int exp;
---
+++ test.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include<sstream>
+using namespace std;
+int main() {
+    int a{0},b{0};
+
+}
